c86317bbd52440706c7dfc9fb95f9064b9b51b77
Nanthish Saravanan <nanthishsaravanan17@gmail.com>
2026-02-07 13:53:38 +0530

    Configure app for Render deployment

--- server/index.js
+++ server/index.js
@@ -36,6 +36,15 @@ app.get('/api/health', (req, res) => {
   res.json({ status: 'ok' });
 });
 
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, '../dist')));
+
+// The "catchall" handler: for any request that doesn't
+// match one above, send back React's index.html file.
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../dist/index.html'));
+});
+
 app.listen(PORT, () => {
   console.log(`Backend running on http://localhost:${PORT}`);
 });
